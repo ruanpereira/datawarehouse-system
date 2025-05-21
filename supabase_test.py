e845f0344127ef13ef6d3c175b93aa182f1e4456
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-05-21 15:21:31 -0300

    db.py

--- supabase_test.py
+++ supabase_test.py
@@ -2,17 +2,13 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-# Carrega as variáveis de ambiente
 load_dotenv()
 
-# Obtém a URL do banco de dados
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Conecta ao banco de dados
 conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
-# Cria a tabela
 cur.execute("""
     CREATE TABLE IF NOT EXISTS vendas (
         id SERIAL PRIMARY KEY,
@@ -26,9 +22,7 @@ cur.execute("""
     );
 """)
 
-# Confirma a transação
 conn.commit()
 
-# Fecha a conexão
 cur.close()
 conn.close()
